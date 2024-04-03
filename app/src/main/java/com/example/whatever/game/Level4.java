5e9e72087b3e7f7f1928728d681227ad8f6c1620
Justin Yung <cwcjustinyung@gmail.com>
2024-04-03 15:29:24 +0800

    Level 3 and 4 Code Completed

--- app/src/main/java/com/example/whatever/game/Level4.java
+++ app/src/main/java/com/example/whatever/game/Level4.java
@@ -1,5 +1,6 @@
 package com.example.whatever.game;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -7,6 +8,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -64,6 +66,7 @@ public class Level4 extends AppCompatActivity implements View.OnTouchListener {
         });
         Enter.setOnClickListener(view -> {
             if(Integer.valueOf(Answer.getText().toString()) == 13){
+                closeKeyboard();
                 onLevelPass();
             }else{
                 onWrongAttempt();
@@ -291,5 +294,12 @@ public class Level4 extends AppCompatActivity implements View.OnTouchListener {
             utils.playSFX(R.raw.tap_sfx);
         }
         return super.dispatchTouchEvent(event);
+    }
+    private void closeKeyboard(){
+        View view = this.getCurrentFocus();
+        if(view != null){
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(view.getWindowToken(),0);
+        }
     }
 }
